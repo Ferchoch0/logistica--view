7611be19c8f9699d9db7cee154aaddca58393f5c
Ferchoch0 <delvalle.fernando.d@gmail.com>
2025-05-14 19:30:32 -0300

    Fix: creacion del diseño del login

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './pages/login'
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  )
+}
+
+export default App
+// import { useState } from 'react'
