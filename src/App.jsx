953557a8501a07551164dad24f245612a14ead8f
Ferchoch0 <delvalle.fernando.d@gmail.com>
2025-05-30 21:28:08 -0300

    fix: rutas creadas y pagina de vehiculos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import DashBoard from './pages/DashBoard'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Vehicles from './pages/Vehicles'
 
 function App() {
